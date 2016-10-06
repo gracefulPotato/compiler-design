c80fef7a40ccc833432941697007476388352ce4
Grace O'Hair-Sherman <gohairsh@be-linux13.lt.ucsc.edu>
2016-10-05 17:02:39 -0700

    trying to get stringstream to read from procline so can intern but segfault

--- 1pa/cppstrtokdir/main.cpp
+++ 1pa/cppstrtokdir/main.cpp
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <wait.h>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 #include "string_set.h"
@@ -17,21 +18,30 @@ int main(int argc, char** argv){
    printf("main.cpp\n");
    string execname = basename (argv[0]);
    int exit_status = EXIT_SUCCESS;
-   string dotstr(basename(argv[1]));
+   string dotstr = basename(argv[1]);
    int dot_index = dotstr.find_last_of(".");
-   dotstr = dotstr.substr(0,dot_index);
-   ofstream myfile;
-   myfile.open(dotstr+".str");
-   myfile<<"test";
+   dotstr = dotstr.substr(0,dot_index)+".str";
+   FILE * outfile = fopen(dotstr.c_str(),"w");
    for (int argi = 1; argi < argc; ++argi){
        string procline;
        pair<string,int> cpp_ret = cpp_line(argi,argv,execname,exit_status);
        cout<<cpp_ret.first;
        cout<<"printing procline"<<procline<<"\n";
-   }
    
+       istringstream input(cpp_ret.first);
+       char* line=0;
+       //for(;;){
+           //if(input.getline(line,1024)){
+              //string_set::intern (line);
+           //}else break;
+           //if(!line) break;
+           //else{
+              //string_set::intern (line);
+           //}
+       //}
+   }
    string_set::intern ("hello");
-   string_set::dump (stdout);
-   myfile.close();
+   string_set::dump (outfile);
+   fclose(outfile);
    return exit_status;
 }
