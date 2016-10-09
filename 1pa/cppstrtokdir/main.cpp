9e79916b0fc49ad87d47d25a3994f78548a1539f
Grace O'Hair-Sherman <gohairsh@unix2.lt.ucsc.edu>
2016-10-08 21:16:49 -0700

    began cleaning up main.cpp's code structure

--- 1pa/cppstrtokdir/main.cpp
+++ 1pa/cppstrtokdir/main.cpp
@@ -17,6 +17,18 @@ using namespace std;
 #include "cppstrtok.h"
 #include "auxlib.h"
 
+string check_filename(string dotstr){
+   cout<<"dotstr: "<<dotstr<<"\n";
+   if(dotstr.find('.')==std::string::npos){
+       fprintf(stderr,"Usage is\noc -ly <program>.oc\nExiting with status 1\n");
+       return "EXIT_FAILURE";
+   }
+   int dot_index = dotstr.find_last_of(".");
+   dotstr=dotstr.substr(0,dot_index)+".str";
+   cout<<"dotstr mark 2: "<<dotstr<<"\n";
+   return dotstr;
+}
+
 int main(int argc, char** argv){
    string execname = basename (argv[0]);
    int exit_status = EXIT_SUCCESS;
@@ -52,37 +64,23 @@ int main(int argc, char** argv){
        }
    }
    int fileindex = argc-1;
-   string dotstr = basename(argv[fileindex]);
-   cout<<"dotstr: "<<dotstr<<"\n";
-   if(!dotstr.find(".")){
-      fprintf(stderr,"no extension, usage\n");
+   string dotstr = check_filename(argv[fileindex]);
+   if(dotstr.compare("EXIT_FAILURE")){
+       return EXIT_FAILURE;
    }
-   int dot_index = dotstr.find_last_of(".");
-   string extension = dotstr.substr(dot_index,dotstr.length()-1);
-   cout<<"extension: "<<extension<<"\n";
-   if(extension!=".oc"){
-       exit_status = EXIT_FAILURE;
+   FILE * outfile = fopen(dotstr.c_str(),"w");
+   string procline;
+   pair<string,int> cpp_ret = cpp_line(fileindex,argv,
+       execname,exit_status,d_args);
+   if(cpp_ret.second==EXIT_FAILURE){
        fprintf(stderr,"Exiting with status %d\n",exit_status);
        return exit_status;
    }
-   dotstr=dotstr.substr(0,dot_index)+".str";
-   cout<<"dotstr mark 2: "<<dotstr<<"\n";
-   FILE * outfile = fopen(dotstr.c_str(),"w");
-   //for (int argi = 1; argi < argc; ++argi){
-       string procline;
-       pair<string,int> cpp_ret = cpp_line(fileindex,argv,
-           execname,exit_status,d_args);
-       if(cpp_ret.second==EXIT_FAILURE){
-             fprintf(stderr,"Exiting with status %d\n",exit_status);
-             return exit_status;
-       }
-       istringstream iss(cpp_ret.first);
-       string line;
-       while(getline(iss,line)){
-              string_set::intern (line.c_str());
-       }
-   //}
-   //string_set::intern ("hello");
+   istringstream iss(cpp_ret.first);
+   string line;
+   while(getline(iss,line)){
+       string_set::intern (line.c_str());
+   }
    string_set::dump (outfile);
    fclose(outfile);
    fprintf(stderr,"Exiting with status %d\n",exit_status);
